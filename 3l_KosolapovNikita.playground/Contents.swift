2fd4ba90f9a029ed8171606947915cca21175082
Nikita Kosolapov <Kosolapovnikitav@gmail.com>
2020-01-29 01:11:54 +0300

    Change some code

--- 3l_KosolapovNikita.playground/Contents.swift
+++ 3l_KosolapovNikita.playground/Contents.swift
@@ -1,26 +1,48 @@
 import UIKit
 
-
-//Общие для структур перечисления
+//Общие для структур (автомобиль и грузовик) перечисления
 
 enum Color {
     case red
     case green
     case blue
     case yellow
+    case purple
+    case orange
 }
 
 enum EngineState {
-       case on
-       case off
-   }
+    case on
+    case off
+}
 
 enum CarModel {
-       case BMW
-       case Mercedes
-       case Volvo
-       case Nissan
-   }
+    case BMW
+    case Mercedes
+    case Volvo
+    case Nissan
+}
+
+enum WindowPosition {
+    case open
+    case close
+}
+      
+enum LampPosition {
+    case on
+    case off
+}
+
+
+//3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+
+enum Actions {
+    case changeEngineState
+    case changeWindowPosition
+    case changeLampPosition
+    case loadTrunk(Double)
+    case unloadTrunk(Double)
+}
 
 
 //1. Описать несколько структур – любой легковой автомобиль и любой грузовик.
@@ -40,87 +62,103 @@ struct Auto {
         case cabriolet
     }
     
-    //3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
-    enum WindowPosition {
-        case open
-        case close
-    }
-        
-    enum LampPosition {
-        case on
-        case off
-    }
-    
-    var status: Class
-    var carModel: CarModel
-    var bodyType: BodyType
-    var color: Color
+    let status: Class
+    let carModel: CarModel
+    let bodyType: BodyType
+    let color: Color
+    var trunkVolume: Double
+    var trunkCondition: Double
+    var yearOfManufacture: Int
     var engineState: EngineState
     var windowPosition: WindowPosition
     var lampPosition: LampPosition
     
-    mutating func changeEngineState() {
-        if engineState == .off {
-            engineState = .on
-        } else {
-            engineState = .off
-        }
-    }
-    
-    mutating func changeWindowPosition() {
-        if windowPosition == .open {
-            windowPosition = .close
-        } else {
-            windowPosition = .open
-        }
-    }
-    
     // 4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
-    mutating func changeLampPosition(lampPosition: LampPosition) {
-        if lampPosition == .on {
-            self.lampPosition = .on
-        } else {
-            self.lampPosition = .off
+    mutating func action (_ action: Actions){
+        switch action {
+        case .changeEngineState:
+              if engineState == .off {
+                engineState = .on
+                print("Двигатель включен")
+                  } else {
+                engineState = .off
+                print("Двигатель отключен")
+                  }
+        case .changeLampPosition:
+            if lampPosition == .off {
+               lampPosition = .on
+                print("Фары включены")
+            } else {
+               lampPosition = .off
+                print("Фары отключены")
+            }
+        case .changeWindowPosition:
+            if windowPosition == .close {
+                windowPosition = .open
+                print("Окна открыты")
+            } else {
+                windowPosition = .close
+                print("Окна закрыты")
+            }
+        case .loadTrunk(let value):
+            if trunkVolume < value {
+                print("Ваш багаж не влезает")
+            } else {
+                trunkCondition += value
+                print("Осталось места в багажнике: \(trunkVolume - trunkCondition) л")
+            }
+        case .unloadTrunk(let value):
+            if trunkCondition - value >= 0 {
+                trunkCondition -= value
+                print("Осталось места в багажнике: \(trunkVolume - trunkCondition) л ")
+            } else {
+                print("Вы не можете выгрузить больше, чем есть")
+            }
         }
     }
-    
+// 6. Вывести значения свойств экземпляров в консоль.
     func description() {
         print("""
-            status: \(self.status)
-            carModel: \(self.carModel)
-            bodyType: \(self.bodyType)
-            color: \(self.color)
-            engineState: \(self.engineState)
-            windowPosition: \(self.windowPosition)
-            lampPosition: \(self.lampPosition) \n
+            \n
+            status: \(status)
+            carModel: \(carModel)
+            bodyType: \(bodyType)
+            color: \(color)
+            engineState: \(engineState)
+            windowPosition: \(windowPosition)
+            lampPosition: \(lampPosition)
+            trunkVolume: \(trunkVolume)
+            trunkCondition: \(trunkCondition)
+            yearOfManufacture: \(yearOfManufacture)\n
             """)
     }
     
-    init(status: Class, carModel: CarModel, bodyType: BodyType, color: Color) {
+    init(status: Class, carModel: CarModel, bodyType: BodyType, color: Color, yearOfManufacture: Int) {
         self.status = status
         self.carModel = carModel
         self.bodyType = bodyType
         self.color = color
-        self.engineState = .off
-        self.windowPosition = .close
-        self.lampPosition = .off
+        self.yearOfManufacture = yearOfManufacture
+        trunkVolume = 500
+        engineState = .off
+        windowPosition = .close
+        lampPosition = .off
+        trunkCondition = 0
     }
 }
 
 //5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
-var car1 = Auto(status: .c_class, carModel:  .BMW, bodyType: .coupe, color: .yellow)
+var car = Auto(status: .c_class, carModel:  .BMW, bodyType: .coupe, color: .yellow, yearOfManufacture: 2015)
 
-car1.description()
-car1.changeLampPosition(lampPosition: .on)
-car1.changeEngineState()
-car1.changeWindowPosition()
+car.description()
+car.action(.changeEngineState)
+car.action(.changeLampPosition)
+car.action(.changeWindowPosition)
+car.action(.loadTrunk(400))
+car.action(.unloadTrunk(300))
+
 
 struct Truck {
-    
-    enum TrunkCondition {
-        case full
-        case empty
-    }
     
     enum TypeOfTruck {
         case Panel
@@ -136,57 +174,98 @@ struct Truck {
         case light
     }
     
-    var carModel: CarModel
-    var maxWeight: MaxWeight
-    var typeOfTruck: TypeOfTruck
-    var trunkCondition: TrunkCondition
+    let typeOfTruck: TypeOfTruck
+    let maxWeight: MaxWeight
+    let carModel: CarModel
+    let color: Color
+    var trunkVolume: Double
+    var trunkCondition: Double
+    var yearOfManufacture: Int
     var engineState: EngineState
-    var color: Color
+    var windowPosition: WindowPosition
+    var lampPosition: LampPosition
     
-    mutating func loadTrunk(){
-        if trunkCondition == .empty {
-            trunkCondition = .full
-        }
-    }
-
-    mutating func unloadTrunk(){
-        if trunkCondition == .full {
-            trunkCondition = .empty
-        }
-    }
-    
-    mutating func changeEngineState() {
-           if engineState == .off {
-               engineState = .on
-           } else {
-               engineState = .off
-           }
-       }
+    mutating func action (_ action: Actions){
+         switch action {
+         case .changeEngineState:
+               if engineState == .off {
+                 engineState = .on
+                 print("Двигатель включен")
+                   } else {
+                 engineState = .off
+                 print("Двигатель отключен")
+                   }
+         case .changeLampPosition:
+             if lampPosition == .off {
+                lampPosition = .on
+                 print("Фары включены")
+             } else {
+                lampPosition = .off
+                 print("Фары отключены")
+             }
+         case .changeWindowPosition:
+             if windowPosition == .close {
+                 windowPosition = .open
+                 print("Окна открыты")
+             } else {
+                 windowPosition = .close
+                 print("Окна закрыты")
+             }
+         case .loadTrunk(let value):
+             if trunkVolume < value {
+                 print("Ваш багаж не влезает")
+             } else {
+                 trunkCondition += value
+                 print("Осталось места в багажнике: \(trunkVolume - trunkCondition) м^3")
+             }
+         case .unloadTrunk(let value):
+             if trunkCondition - value >= 0 {
+                 trunkCondition -= value
+                 print("Осталось места в багажнике: \(trunkVolume - trunkCondition) м^3")
+             } else {
+                 print("Вы не можете выгрузить больше, чем есть")
+             }
+         }
+     }
     
     func description() {
-       print("""
-           carModel: \(self.carModel)
-           typeOfTruck: \(self.typeOfTruck)
-           maxWeight: \(self.maxWeight)
-           trunkCondition: \(self.trunkCondition)
-           color: \(self.color)
-           engineState: \(self.engineState) \n
-           """)
+        print("""
+            \n
+            carModel: \(carModel)
+            typeOfTruck: \(typeOfTruck)
+            maxWeight: \(maxWeight)
+            color: \(color)
+            engineState: \(engineState)
+            windowPosition: \(windowPosition)
+            lampPosition: \(lampPosition)
+            trunkVolume: \(trunkVolume)
+            trunkCondition: \(trunkCondition)
+            yearOfManufacture: \(yearOfManufacture)\n
+            """)
        }
     
-    init(maxWeight: MaxWeight, typeOfTruck: TypeOfTruck, carModel: CarModel, color: Color) {
-        self.carModel = carModel
+    init(maxWeight: MaxWeight, typeOfTruck: TypeOfTruck, carModel: CarModel, color: Color, yearOfManufacture: Int) {
         self.maxWeight = maxWeight
-        self.color = color
-        self.engineState = .off
-        self.trunkCondition = .empty
         self.typeOfTruck = typeOfTruck
+        self.carModel = carModel
+        self.color = color
+        self.yearOfManufacture = yearOfManufacture
+        trunkVolume = 100
+        engineState = .off
+        windowPosition = .close
+        lampPosition = .off
+        trunkCondition = 0
     }
 }
 
-var truck1 = Truck(maxWeight: .heavy, typeOfTruck: .Dump, carModel: .BMW, color: .green)
+var truck = Truck(maxWeight: .heavy, typeOfTruck: .Dump, carModel: .BMW, color: .green, yearOfManufacture: 2005)
 
-truck1.description()
-truck1.changeEngineState()
-truck1.loadTrunk()
+truck.description()
+truck.action(.changeEngineState)
+truck.action(.changeLampPosition)
+truck.action(.changeWindowPosition)
+truck.action(.loadTrunk(70))
+truck.action(.unloadTrunk(30))
+
+
 
